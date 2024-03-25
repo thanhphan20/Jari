24a4fc32a3a3277b6f332564b6849dac7d8ee167
thanhphan20 <phancongthanh2307199@gmail.com>
2024-03-25 21:41:22 +0700

    First commit

---
+++ src/test/java/com/example/jari/JariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
